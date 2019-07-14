f096e3e16e930bdfc832ef4592e258c45ae89457
Heikki Kainulainen <hmkainul@gmail.com>
2019-07-14 19:25:22 +0300

    Mock  elements service

--- backend/src/test/java/com/example/elements/boundary/ElementsTest.java
+++ backend/src/test/java/com/example/elements/boundary/ElementsTest.java
@@ -1,21 +1,35 @@
 package com.example.elements.boundary;
 
-import static org.junit.Assert.assertEquals;
 import java.math.BigDecimal;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
 import org.junit.Test;
+import static org.mockito.Mockito.*;
 import com.example.elements.entity.Element;
 
 public class ElementsTest {
 
-    Elements service = new Elements();
+    private static final int NUMBER = 1;
+    private static final String SYMBOL = "H";
+    private static final String NAME = "Hydrogen";
+    private static final BigDecimal WEIGHT = new BigDecimal("1.008");
+
+    Elements service;
+
+    @Before
+    public void setup() {
+        service = mock(Elements.class);
+        Element hydrogen = new Element(NUMBER, SYMBOL, NAME, WEIGHT);
+        when(service.get(NUMBER)).thenReturn(hydrogen);
+    }
 
     @Test
     public void getHydrogen() {
-        Element e = service.get(1);
-        assertEquals(1, e.getNumber());
-        assertEquals("H", e.getSymbol());
-        assertEquals("Hydrogen", e.getName());
-        assertEquals(new BigDecimal("1.008"), e.getWeight());
+        Element e = service.get(NUMBER);
+        assertEquals(NUMBER, e.getNumber());
+        assertEquals(SYMBOL, e.getSymbol());
+        assertEquals(NAME, e.getName());
+        assertEquals(WEIGHT, e.getWeight());
     }
 
 }
