0a812d63e5e065c88f6d0e19ebb7f0f822fe7be6
Heikki Kainulainen <hmkainul@gmail.com>
2019-08-20 20:32:25 +0300

    Remove docker

--- systemtest/buildAndRun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mvn clean package && docker build -t com.example/systemtest .
-docker rm -f systemtest || true && docker run -d -p 8080:8080 -p 4848:4848 --name systemtest com.example/systemtest 
